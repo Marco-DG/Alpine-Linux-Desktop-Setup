1c9c4fe6aa40379e1d2e5a02864e82894f21321d
Marco-DG <dg.account@protonmail.com>
2023-08-26 10:58:17 +0200

    .

--- 01_edge-main-repo.sh
+++ 01_edge-main-repo.sh
@@ -2,9 +2,11 @@
 truncate -s0 /etc/apk/repositories
 
 # write the repositories
-echo "https://dl-cdn.alpinelinux.org/alpine/edge/main" >> /etc/apk/repositories
-echo "# https://dl-cdn.alpinelinux.org/alpine/edge/community" >> /etc/apk/repositories
-echo "# https://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
+{
+        echo "https://dl-cdn.alpinelinux.org/alpine/edge/main"
+        echo "# https://dl-cdn.alpinelinux.org/alpine/edge/community"
+        echo "# https://dl-cdn.alpinelinux.org/alpine/edge/testing"
+} >> /etc/apk/repositories
 
 # update the system
 doas apk update
