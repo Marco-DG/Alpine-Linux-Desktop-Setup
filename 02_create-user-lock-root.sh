2b01130180a4ff571ae40a369efd254e72ef5e08
Marco-DG <dg.account@protonmail.com>
2023-08-26 14:14:59 +0200

    .

--- 02_create-user-lock-root.sh
+++ 02_create-user-lock-root.sh
@@ -1,25 +1,31 @@
 # run this script as root
 
-# -n 
-#      do not output a trailing newline.
-echo -n "Enter the username: "
-read -r _username
+_create-user-lock-root()
+{
+    # -n 
+    #      do not output a trailing newline.
+    echo -n "Enter the username: "
+    read -r _username
 
-if "$_username" | grep -E '^[a-z_]([a-z0-9_-]{0,31}|[a-z0-9_-]{0,30}\$)$' -q; then
-    exit 1
-fi
+    if "$_username" | grep -E '^[a-z_]([a-z0-9_-]{0,31}|[a-z0-9_-]{0,30}\$)$' -q; then
+        return 1
+    fi
 
-echo -n "Enter the password: "
-read -s _password
+    echo -n "Enter the password: "
+    read -s _password
 
-adduser --home /home/"$_username" $_username # --shell bash
+    adduser --home /home/"$_username" $_username # --shell bash
 
-#--stdin
-#      This option is used to indicate that passwd should read the new
-#      password from standard input, which can be a pipe.
-echo "$_password" | passwd "$_username" --stdin
+    #--stdin
+    #      This option is used to indicate that passwd should read the new
+    #      password from standard input, which can be a pipe.
+    echo "$_password" | passwd "$_username" --stdin
 
-adduser $_username wheel
+    adduser $_username wheel
 
-unset _username
-unset _password
+    unset _username
+    unset _password
+}
+
+_create-user-lock-root
+unset _create-user-lock-root
