8e1863da67e5a15f6408e74928aef1f9dbe5c185
Marco-DG <dg.account@protonmail.com>
2023-08-28 22:48:01 +0200

    .

--- 05_ash-config.sh
+++ 05_ash-config.sh
@@ -7,6 +7,8 @@ if [ "$(id -u)" -eq 0 ]
     exit 1
 fi
 
+_home=$HOME
+
 ###############################################################
 ###                 edit /etc/profile                       ###
 ###############################################################
@@ -33,7 +35,7 @@ doas truncate -s0 /etc/profile
 {
     echo 'export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"'
     echo ""
-    echo "export ENV=\$HOME/.configs/ash/.ashrc"
+    echo "export ENV=\$_home/.configs/ash/.ashrc"
     echo ""
     echo "for script in /etc/profile.d/*.sh ; do"
     echo -e -n "\t"; echo   'if [-r "\$script" ] ; then'
@@ -52,20 +54,21 @@ EOF
 ###############################################################
 ###             ~/.configs/ash/.ashrc                       ###
 ###############################################################
-mkdir -p $HOME/.configs/ash/
-wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/.configs/ash/.ashrc  -P $HOME/.configs/ash/
+mkdir -p $_home/.configs/ash/
+wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/.configs/ash/.ashrc  -P $_home/.configs/ash/
 
 ###############################################################
 ###             ~/.logs/ash/.ash_history                    ###
 ###############################################################
-mkdir -p $HOME/.logs/ash
-mv -f $HOME/.ash_history $HOME/.logs/ash/
+mkdir -p $_home/.logs/ash
+mv -f $_home/.ash_history $_home/.logs/ash/
 
 exec ash
-rm -f $HOME/.ash_history
+rm -f $_home/.ash_history
 
 #doas -u $(id -u -n) ash << EOF
-#    rm -f $HOME/.ash_history
+#    rm -f $_home/.ash_history
 #EOF
 
+unset _home
 unset _to_nuke_or_not_to_nuke_that_is_the_question
