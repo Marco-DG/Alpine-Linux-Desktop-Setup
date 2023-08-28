b78b57bcd6aa7cf11d7d9ea099f5cb229bb71cc3
Marco-DG <dg.account@protonmail.com>
2023-08-28 16:49:23 +0200

    .

---
+++ 05_ash_config.sh
@@ -0,0 +1,73 @@
+################################################################
+###             Check that user is not root                 ####
+################################################################
+
+if [ "$(id -u)" -eq 0 ]
+    then echo "This script cannot be executed by root"
+    exit 1
+fi
+
+################################################################
+###                .ashrc in ~.configs/ash/                 ####
+################################################################
+
+###############################################
+### create .ashrc                           ###
+###############################################
+touch $HOME/.configs/ash/.ashrc
+
+###############################################
+### nuke /etc/profile                       ###
+###############################################
+echo "ATTENTION !!! '/etc/profile/ will be nuked, digit "y" to continue, any other key will exit: "
+read _to_nuke_or_not_to_nuke_that_is_the_question
+if "$_to_nuke_or_not_to_nuke_that_is_the_question"="y"; then
+    echo ""
+    echo "Please provide root access: "
+    doas -u root ash << EOF
+    doas truncate -s0 /etc/profile
+else
+    exit 1
+fi
+unset _to_nuke_or_not_to_nuke_that_is_the_question
+
+###############################################
+### write /etc/profile                      ###
+###############################################
+{
+    echo 'export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"'
+    echo ""
+    echo "export ENV=\$HOME/.configs/ash/.ashrc"
+    echo ""
+    echo "for script in /etc/profile.d/*.sh ; do"
+    echo -e -n "\t"; echo   'if [-r "\$script" ] ; then'
+    echo -e -n "\t\t"; echo     '. "\$script"'
+    echo -e -n "\t"; echo   "fi"
+    echo "done"
+    echo "unset script"
+    echo ""
+} >> /etc/profile
+
+###############################################
+### drop root privileges                    ###
+###############################################
+EOF
+
+################################################################
+###               .ash_history to ~.logs/ash/               ####
+################################################################
+
+###############################################
+### set HISTFILE enviroment variable        ###
+###############################################
+mkdir $HOME/.logs/ash
+{
+    echo 'HISTFILE="$HOME/.config/ash_history"'
+} >> $HOME/.configs/.ashrc
+
+###############################################
+### move .ash_history                       ###
+###############################################
+mv ~.ash_history .logs/ash/.ash_history
+
+exec ash -l
