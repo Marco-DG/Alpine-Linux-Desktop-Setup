dc2c331161c73b96e53d739511e6590d2c66e602
Marco-DG <dg.account@protonmail.com>
2023-08-28 10:40:13 +0200

    .

--- 04_silent-boot.sh
+++ 04_silent-boot.sh
@@ -26,7 +26,7 @@ fi
 if grep -q "openrc boot --quiet" /etc/inittab; then
     echo "WARNING: the file '/etc/inittab' alredy contains the string 'openrc boot --quiet', as such the script will not commit any changes and it adivised to check the '/etc/inittab' integrity manually."
 else
-    doas sed -i 's@openrc boot@openrc boot --quiet@g' /etc/inittab
+    doas sed -i 's@openrc boot@openrc boot --quiet @g' /etc/inittab
 fi
 
 # openrc default
@@ -51,17 +51,13 @@ fi
 # see: https://unix.stackexchange.com/questions/579013/how-to-improve-startup-time-of-openrc-system
 #
 # even tough many have reported speedups due to parallelization,
-# personally I didn't experience any noticeable improvement,
-# for the moment I will leave it single threaded
-#
-# IMPORTANT: this section is a sketch, I didn't test it not even once.
+# personally I didn't experience any noticeable improvement
 #
 # INFO: when 'rc_parallel = "YES"' the aesthetic changes, the text is prompted differently.
 
-#### RUN OPEN-RC IN PARALLEL
-#if grep -q \#rc_parallel=\"NO\" /etc/rc.conf; then
-#    # uses "@" as a delimiter; the '-i' flag edits the file in-place
-#    doas sed -i 's@#rc_parallel="NO"@rc_parallel="YES"@g' /etc/inittab
-#else
-#    echo "WARNING: unable to find the string '#rc_parallel=\"NO\"'"
-#fi
+if grep -q \#rc_parallel=\"NO\" /etc/rc.conf; then
+    # uses "@" as a delimiter; the '-i' flag edits the file in-place
+    doas sed -i 's@#rc_parallel="NO"@rc_parallel="YES"@g' /etc/inittab
+else
+    echo "WARNING: unable to find the string '#rc_parallel=\"NO\"'"
+fi
