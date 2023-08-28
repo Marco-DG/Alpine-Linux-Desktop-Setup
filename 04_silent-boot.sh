bdda3b324acb3a3971f2d5e7c0e7aed47fbb77ab
Marco-DG <dg.account@protonmail.com>
2023-08-28 14:26:23 +0200

    .

--- 04_silent-boot.sh
+++ 04_silent-boot.sh
@@ -1,15 +1,6 @@
 ### How:
 #       by adding the '--quiet' flag to each '/sbin/openrc' command within '/etc/inittab'
 
-### NOTES
-#   the --quiet flag alone does not suppress all the messages:
-#       - errors are still displayed, to disable them, repeat the flag twice: --quiet --quiet
-#       - fcsk still display messages, to disable them, redirect them to /dev/null:
-#           openrc boot --quiet >> /dev/null
-#       - networking message are still displayed, I don't know hot to disable them
-#       - busybox (I think) messages are still displayed during shutdown and reboot
-#       - Welcome to Alpine is still displayed, to disable it truncate /etc/issue
-
 ################################################################
 ###                 Check root privileges                   ####
 ################################################################
@@ -17,6 +8,25 @@
 if [ "$(id -u)" -ne 0 ]
     then echo "This script requires root privileges"
     exit 1
+fi
+
+################################################################
+###                 Parallelizing Boot                      ####
+################################################################
+
+### NOTE on 'Parallelize Boot':
+# see: https://unix.stackexchange.com/questions/579013/how-to-improve-startup-time-of-openrc-system
+#
+# even tough many have reported speedups due to parallelization,
+# personally I didn't experience any noticeable improvement
+#
+# INFO: when 'rc_parallel = "YES"' the aesthetic changes, the text is prompted differently.
+
+if grep -q \#rc_parallel=\"NO\" /etc/rc.conf; then
+    # uses "@" as a delimiter; the '-i' flag edits the file in-place
+    doas sed -i 's@#rc_parallel="NO"@rc_parallel="YES"@g' /etc/rc.conf
+else
+    echo "WARNING: unable to find the string '#rc_parallel=\"NO\"' in /etc/rc.conf"
 fi
 
 ################################################################
@@ -52,21 +62,14 @@ else
     doas sed -i 's@openrc shutdown@openrc shutdown --quiet@g' /etc/inittab
 fi
 
-################################################################
-###                 Parallelizing Boot                      ####
-################################################################
+### NOTES
+#   the --quiet flag alone does not suppress all the messages:
+#       - errors are still displayed, to disable them, repeat the flag twice: --quiet --quiet (according to the documentation, difficult to test)
+#       - fcsk still display messages, to disable them, redirect them to /dev/null (or perhaps it would be better to log them) :
+#           openrc boot --quiet >> /dev/null
+#       - networking message are still displayed, I don't know hot to disable them
+#       - busybox messages are still displayed during shutdown and reboot because they are hardcoded in the binary, more info at:
+#           https://github.com/mirror/busybox/blob/master/init/init.c
+#           https://github.com/open-power/op-build/issues/2385
+#       - "Welcome to Alpine" is defined in /etc/issue, to disable it truncate the file (In my case the current configuration already hides it)
 
-### NOTE on 'Parallelize Boot':
-# see: https://unix.stackexchange.com/questions/579013/how-to-improve-startup-time-of-openrc-system
-#
-# even tough many have reported speedups due to parallelization,
-# personally I didn't experience any noticeable improvement
-#
-# INFO: when 'rc_parallel = "YES"' the aesthetic changes, the text is prompted differently.
-
-if grep -q \#rc_parallel=\"NO\" /etc/rc.conf; then
-    # uses "@" as a delimiter; the '-i' flag edits the file in-place
-    doas sed -i 's@#rc_parallel="NO"@rc_parallel="YES"@g' /etc/rc.conf
-else
-    echo "WARNING: unable to find the string '#rc_parallel=\"NO\"' in /etc/rc.conf"
-fi
