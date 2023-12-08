f6a05d00e25f75e3b33cf5f2fcb0682c5efd9815
Marco D.G <39568183+Marco-DG@users.noreply.github.com>
2023-12-08 15:24:09 +0100

    rename "configs" into "config"

--- 05_ash-config.sh
+++ 05_ash-config.sh
@@ -35,7 +35,7 @@ doas truncate -s0 /etc/profile
 {
     echo 'export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"'
     echo ""
-    echo "export ENV=$_home/.configs/ash/.ashrc"
+    echo "export ENV=$_home/.config/ash/.ashrc"
     echo ""
     echo "for script in /etc/profile.d/*.sh ; do"
     echo -e -n "\t"; echo   'if [ -r "\$script" ] ; then'
@@ -58,11 +58,11 @@ doas truncate -s0 /etc/profile
 EOF
 
 ###############################################################
-###                 ~/.configs/ash/.ashrc                   ###
+###                 ~/.config/ash/.ashrc                   ###
 ###############################################################
-mkdir -p $_home/.configs/ash/
-rm -f $_home/.configs/ash/.ashrc
-wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/.configs/ash/.ashrc  -P $_home/.configs/ash/
+mkdir -p $_home/.config/ash/
+rm -f $_home/.config/ash/.ashrc
+wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/.config/ash/.ashrc  -P $_home/.config/ash/
 
 ###############################################################
 ###             ~/.logs/ash/.ash_history                    ###
