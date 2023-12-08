85cad282e66584f89a9e2e35ff3c9b5dd5410e6e
Marco D.G <39568183+Marco-DG@users.noreply.github.com>
2023-12-08 10:48:19 +0100

    .

---
+++ 00_wget_scripts.sh
@@ -0,0 +1,8 @@
+mkdir Alpine-Linux-Desktop-Setup
+cd Alpine-Linux-Desktop-Setup
+
+wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/01_edge-main-repo.sh
+wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/02_create-user-lock-root.sh
+wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/03_autologin.sh
+wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/04_silent-boot.sh
+wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/05_ash-config.sh
