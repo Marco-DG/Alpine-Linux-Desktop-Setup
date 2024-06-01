d6af60df202d02a9656b6c9bc0e05939781bfc49
Marco D.G <39568183+Marco-DG@users.noreply.github.com>
2024-06-01 19:27:58 +0200

    .

--- kakoune.sh
+++ kakoune.sh
@@ -16,3 +16,15 @@ mkdir -p ~/.config/kak/colors
 wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/.config/kak/kakrc -P ~/.config/kak/
 wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/.config/kak/colors/kak.kak -P ~/.config/kak/colors/
 wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/.config/kak/colors/kak-in.kak -P ~/.config/kak/colors/
+
+#plugins
+mkdir -p ~/.config/kak/plugins
+
+# plug.kak
+mkdir -p ~/.config/kak/plugins/plug.kak
+wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/.config/kak/plugins/plug.kak/README.md -P ~/.config/kak/plugins/plug.kak/
+wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/.config/kak/plugins/plug.kak/LICENSE -P ~/.config/kak/plugins/plug.kak/
+
+mkdir -p ~/.config/kak/plugins/plug.kak/rc
+wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/.config/kak/plugins/plug.kak/rc/plug.sh -P ~/.config/kak/plugins/plug.kak/rc/
+wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/.config/kak/plugins/plug.kak/rc/plug.kak -P ~/.config/kak/plugins/plug.kak/rc/
