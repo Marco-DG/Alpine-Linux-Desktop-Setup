8ff20290ea1631a5c051022c9136a51a5749641e
Marco D.G <39568183+Marco-DG@users.noreply.github.com>
2024-06-01 18:29:08 +0200

    .

--- 00_wget_scripts.sh
+++ 00_wget_scripts.sh
@@ -1,11 +1,11 @@
 rm -rf ~/scripts/
 mkdir -p ~/scripts/
 
-wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/01_edge-main-repo.sh -O ~/scripts/
-wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/02_create-user-lock-root.sh -O ~/scripts/
-wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/03_autologin.sh -O ~/scripts/
-wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/04_silent-boot.sh -O ~/scripts/
-wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/05_ash-config.sh -O ~/scripts/
-wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/06_labwc.sh -O ~/scripts/
-wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/07_brightnessctl.sh -O ~/scripts/
-wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/kakoune.sh -O ~/scripts/
+wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/01_edge-main-repo.sh -P ~/scripts/
+wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/02_create-user-lock-root.sh -P ~/scripts/
+wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/03_autologin.sh -P ~/scripts/
+wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/04_silent-boot.sh -P ~/scripts/
+wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/05_ash-config.sh -P ~/scripts/
+wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/06_labwc.sh -P ~/scripts/
+wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/07_brightnessctl.sh -P ~/scripts/
+wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/kakoune.sh -P ~/scripts/
