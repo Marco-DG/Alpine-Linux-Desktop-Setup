4ca79c4b06f43822d552a12adf01280440339736
Marco D.G <39568183+Marco-DG@users.noreply.github.com>
2024-06-01 18:27:07 +0200

    .

--- 00_wget_scripts.sh
+++ 00_wget_scripts.sh
@@ -1,8 +1,11 @@
-wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/01_edge-main-repo.sh
-wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/02_create-user-lock-root.sh
-wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/03_autologin.sh
-wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/04_silent-boot.sh
-wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/05_ash-config.sh
-wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/06_labwc.sh
-wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/07_brightnessctl.sh
-wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/kakoune.sh
+rm -rf ~/scripts/
+mkdir -p ~/scripts/
+
+wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/01_edge-main-repo.sh -O ~/scripts/
+wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/02_create-user-lock-root.sh -O ~/scripts/
+wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/03_autologin.sh -O ~/scripts/
+wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/04_silent-boot.sh -O ~/scripts/
+wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/05_ash-config.sh -O ~/scripts/
+wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/06_labwc.sh -O ~/scripts/
+wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/07_brightnessctl.sh -O ~/scripts/
+wget https://raw.githubusercontent.com/Marco-DG/Alpine-Linux-Desktop-Setup/master/kakoune.sh -O ~/scripts/
